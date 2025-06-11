633493cbb588345942216c5e348d1b3886bb4df7
mvmories <mvmories@gmail.com>
2025-06-11 19:12:19 +0200

    Add exception and usage guidelines via sys.argv length condition

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ from stats import (
 )
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        raise Exception("Usage: python3 main.py <path_to_book>")
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
     chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
     print_report(book_path, num_words, chars_sorted_list)
+
+
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -30,4 +36,8 @@ def print_report(book_path, num_words, chars_sorted_list):
     print("============= END ===============")
 
 
-main()
+try:
+    main()
+except Exception as e:
+    print(e)
+    sys.exit(1)
