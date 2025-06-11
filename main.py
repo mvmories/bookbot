faa447c193cebb66b9fbceae121b3a1129a38b32
mvmories <mvmories@gmail.com>
2025-06-11 18:25:36 +0200

    Refactor code and add print report function

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-from stats import get_num_words, get_num_chars
+import sys
+from stats import (
+    get_num_words,
+    get_chars_dict,
+    chars_dict_to_sorted_list
+)
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
 
-def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("----------- Character Count ----------")
+    for dict in chars_sorted_list:
+        if dict["char"].isalpha():
+            print(f"{dict["char"]}: {dict["num"]}")
+    print("============= END ===============")
 
-    # Count total words in book
-    num_word = get_num_words(book_text)
-    word_count = f"{num_word} words found in the document"
-
-    # Count chars in book
-    char_count = get_num_chars(book_text)
-
-    print(word_count)
-    print(char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 def get_num_words(book_text):
     return len(book_text.split())
 
-def get_num_chars(book_text):
+def get_chars_dict(book_text):
     char_dict = {}
     for char in book_text:
         lower_char = char.lower()
@@ -10,3 +10,15 @@ def get_num_chars(book_text):
         else:
             char_dict[lower_char] = 1
     return char_dict
+
+def sort_on(dict):
+    return dict["num"]
+
+def chars_dict_to_sorted_list(num_char_dict):
+    sorted_list = []
+    for char in num_char_dict:
+        count = num_char_dict[char]
+        dict = {"char": char, "num": count}
+        sorted_list.append(dict)
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
